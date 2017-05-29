22128a5f8ff1b807a2d37579e271f9c3d6870032
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-05-29 23:45:39 +1200

    Done

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// @flow
+
+import Immutable, { Map } from 'immutable';
+import commandLineArgs from 'command-line-args';
+import fs from 'fs';
+import csv from 'fast-csv';
+import { StapleTemplateShoppingListService, TagService } from 'smart-grocery-parse-server-common';
+import './bootstrap';
+
+const optionDefinitions = [{ name: 'csvFilePath', type: String }];
+const options = commandLineArgs(optionDefinitions);
+
+const loadAllTags = async () => {
+  const result = await TagService.search(Map({ limit: 1000 }));
+
+  return result;
+};
+
+const start = async () => {
+  const allTags = await loadAllTags();
+
+  fs
+    .createReadStream(options.csvFilePath)
+    .pipe(csv.parse({ headers: false }))
+    .pipe(csv.format({ headers: false }))
+    .transform(async (rarRow, next) => {
+      const row = Immutable.fromJS(rarRow);
+      const description = row.first().trim();
+      const tags = row.skip(1).map(_ => _.trim()).toSet();
+
+      if (tags.filterNot(_ => allTags.find(tag => tag.get('name').localeCompare(_) === 0)).isEmpty()) {
+        await StapleTemplateShoppingListService.create(
+          Map({
+            description,
+            tagIds: tags.map(_ => allTags.find(tag => tag.get('name').localeCompare(_) === 0).get('id')),
+          }),
+        );
+        next();
+      } else {
+        console.log(`Provided tags not found: ${row.skip(1).toJS()}`);
+        next(`Provided tags not found: ${row.skip(1).toJS()}`);
+      }
+    });
+};
+
+start();
